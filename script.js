81958d3dd044fba9ef9466d8a9375378e4feb02c
Léo Geison <leogeisom@gmail.com>
2023-02-13 23:21:35 -0300

    creating countdown logic and updating cards

--- script.js
+++ script.js
@@ -10,8 +10,39 @@ const cardRain = document.querySelector('.rain')
 const cardFirePlace = document.querySelector('.fireplace')
 const cardCoffeShop = document.querySelector('.coffe-shop')
 
+play.addEventListener('click', function () {
+  play.classList.add('hide')
+  pause.classList.remove('hide')
+  coutdown()
+})
 
+pause.addEventListener('click', function () {
+  play.classList.remove('hide')
+  pause.classList.add('hide')
+})
 
+stop.addEventListener('click', function () {
+  play.classList.remove('hide')
+  pause.classList.add('hide')
+})
 
-
-
+function coutdown() {
+  setTimeout(function () {
+    let seconds = Number(secondsDisplay.textContent)
+    let minutes = Number(minutesDisplay.textContent)
+    
+    if (minutes <= 0 && seconds <= 0) {
+      play.classList.remove('hide')
+      pause.classList.add('hide')
+      
+      return
+    }
+    if (seconds <= 0) {
+      seconds = 02
+      
+      minutesDisplay.textContent = String(minutes - 1).padStart(2, '0')
+    }
+    secondsDisplay.textContent = String(seconds - 1).padStart(2, '0')
+    coutdown()
+  }, 1000)
+}
