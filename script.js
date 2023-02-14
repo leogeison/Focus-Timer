ef2c2e336839455a5e2a2c9c42959e0fe59f2964
Léo Geison <leogeisom@gmail.com>
2023-02-13 21:32:12 -0300

    adding DOM elements in JS

---
+++ script.js
@@ -0,0 +1,17 @@
+const play = document.querySelector('.play')
+const pause = document.querySelector('.pause')
+const stop = document.querySelector('.stop')
+const increaseSound = document.querySelector('.increase-sound')
+const decreaseSound = document.querySelector('.decrease-sound')
+const minutesDisplay = document.querySelector('.minutes')
+const secondsDisplay = document.querySelector('.seconds')
+const cardForest = document.querySelector('.forest')
+const cardRain = document.querySelector('.rain')
+const cardFirePlace = document.querySelector('.fireplace')
+const cardCoffeShop = document.querySelector('.coffe-shop')
+
+
+
+
+
+
